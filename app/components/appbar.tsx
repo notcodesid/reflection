28018946b1abf1f7f3e8376db10e1963dc4c40f5
siddharth <notcodesid@gmail.com>
2024-12-17 19:15:42 +0530

    add links

--- app/components/appbar.tsx
+++ app/components/appbar.tsx
@@ -7,44 +7,48 @@ export default function Appbar() {
     return (
 
         <div className="flex justify-between">
+            <Link href={"/"} >
+         
 
-         <nav className="flex items-center justify-between px-6 py-4">
+            <nav className="flex items-center justify-between px-6 py-4">
                 <div className="flex items-center gap-2">
-                  <Image
-                    src="https://pbs.twimg.com/profile_images/1801972870756925440/i5Hk08rP_400x400.jpg"
-                    alt="Logo"
-                    width={32}
-                    height={32}
-                    className="rounded"
+                    <Image
+                        src="https://pbs.twimg.com/profile_images/1801972870756925440/i5Hk08rP_400x400.jpg"
+                        alt="Logo"
+                        width={32}
+                        height={32}
+                        className="rounded"
                     />
-                  <span className="text-lg font-medium">Awakenings - Siddharth </span>
+                    <span className="text-lg font-medium">Awakenings - Siddharth </span>
                 </div>
-              </nav>
+            </nav>
 
-              <div className="p-5">
-          
-          <div className="flex gap-2 justify-center">
-            <Link href="https://x.com/notcodesid" className="bg-black rounded-full p-2 hover:opacity-80">
-              <Image
-                src={XLogo}
-                alt="Profile"
-                width={20}
-                height={20}
-                className="mx-auto rounded-full"
-                priority
-              />
-              <span className="sr-only">X (formerly Twitter)</span>
             </Link>
-            <Link href="https://github.com/notcodesid" className="bg-black rounded-full p-2 hover:opacity-80">
-              <Github className="h-5 w-5 text-white" />
-              <span className="sr-only">GitHub</span>
-            </Link>
-            <Link href="https://www.linkedin.com/in/sidharthh-pandro/" className="bg-black rounded-full p-2 hover:opacity-80">
-              <Linkedin className="h-5 w-5 text-white" />
-              <span className="sr-only">LinkedIn</span>
-            </Link>
-          </div>
+
+            <div className="p-5">
+
+                <div className="flex gap-2 justify-center">
+                    <Link href="https://x.com/notcodesid" className="bg-black rounded-full p-2 hover:opacity-80">
+                        <Image
+                            src={XLogo}
+                            alt="Profile"
+                            width={20}
+                            height={20}
+                            className="mx-auto rounded-full"
+                            priority
+                        />
+                        <span className="sr-only">X (formerly Twitter)</span>
+                    </Link>
+                    <Link href="https://github.com/notcodesid" className="bg-black rounded-full p-2 hover:opacity-80">
+                        <Github className="h-5 w-5 text-white" />
+                        <span className="sr-only">GitHub</span>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/sidharthh-pandro/" className="bg-black rounded-full p-2 hover:opacity-80">
+                        <Linkedin className="h-5 w-5 text-white" />
+                        <span className="sr-only">LinkedIn</span>
+                    </Link>
+                </div>
+            </div>
         </div>
-                    </div>
     )
 }
