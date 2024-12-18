6f1569e331387cdab8f69b591f1ce88a09a16f40
siddharth <notcodesid@gmail.com>
2024-12-18 15:26:30 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image"
-import Link from "next/link"
-import { Linkedin, Github } from 'lucide-react'
 import Appbar from "./components/appbar"
 import BlogSection from "./components/blog-section"
 
