3ee03ea16ff634027ee4419275a4e037933b63bf
siddharth <notcodesid@gmail.com>
2024-12-23 12:25:16 -0500

    update layout with open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,31 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Awakenings - Siddharth",
   description: "Snapshots of growth, reflections, and insights for curious minds navigating life, tech, and everything in between.",
+
+  // open graph
+  openGraph : {
+    title : ' Awakenings - Siddharth',
+    description : 'Snapshots of growth, reflections, and insights for curious minds navigating life, tech, and everything in between.',
+    url : 'localhost:3000',
+    siteName : 'Awakening',
+    images : [
+      {
+        url : 'https://drive.google.com/file/d/1kxUsg4r5-0Oe5p33SsV47xBG_cXqr3kY/view?usp=drive_link', // image link
+        width : 1200,
+        height : 500,
+        alt : 'Title with their description'
+      },
+    ],
+    locale : 'en_US',
+    type:'article'
+  },
+
+  twitter : {
+    card : 'summary_large_image',
+    title : ' Awakenings - Siddharth',
+    description : 'Snapshots of growth, reflections, and insights for curious minds navigating life, tech, and everything in between.',
+   images : ['https://drive.google.com/file/d/1kxUsg4r5-0Oe5p33SsV47xBG_cXqr3kY/view?usp=drive_link'] // image link
+  }
 };
 
 export default function RootLayout({
