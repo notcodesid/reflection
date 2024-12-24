12e94baaae148131a92759d44304ffb88c4d9338
siddharth <notcodesid@gmail.com>
2024-12-24 12:09:49 -0500

    remove markdown blogs

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -13,52 +13,12 @@ interface BlogPost {
 
 const samplePosts: BlogPost[] = [
   {
-    id: "a-new-beginning",
-    title: "A New Beginning",
-    date: "December 16, 2024",
-    likes: 8,
-    image:
-      "https://i.pinimg.com/736x/7c/0b/d4/7c0bd435745921a61079abb7ef8b0c5a.jpg",
-  },
-  {
-    id: "2",
-    title: "The Art of Problem Solving",
-    date: "December 18, 2024",
-    likes: 12,
-    image:
-      "https://i.pinimg.com/736x/b0/55/74/b05574e5afd4d182d2f8e881dfe0e18a.jpg",
-  },
-  {
-    id: "3",
-    title: "Mindfulness in the Digital Age",
-    date: "December 20, 2024",
-    likes: 15,
-    image:
-      "https://i.pinimg.com/736x/d8/28/70/d82870ec84038587dc3c9c4726ab7463.jpg",
-  },
-  {
-    id: "4",
-    title: "The Future of AI",
-    date: "December 22, 2024",
-    likes: 20,
-    image:
-      "https://i.pinimg.com/736x/7d/c7/e4/7dc7e41d715fb091f1fefc4cd5c00c6c.jpg",
-  },
-  {
-    id: "5",
-    title: "Lessons from Failure",
-    date: "December 24, 2024",
+    id: "how-to-get-hire",
+    title: "How to get hire - Sam Altman",
+    date: "September 09, 2024",
     likes: 18,
     image:
       "https://i.pinimg.com/736x/5a/d4/71/5ad471a2ba34058c37a640ed92db7e19.jpg",
-  },
-  {
-    id: "6",
-    title: "The Power of Habit",
-    date: "December 26, 2024",
-    likes: 22,
-    image:
-      "https://i.pinimg.com/736x/94/b4/54/94b454f14c145475b78da427122b29bb.jpg",
   },
 ];
 
